e898e3ad284e22f34e5f16b45e6cf54f1c98a650
玉度 <lianghonghui.lhh@alibaba-inc.com>
2023-03-07 11:03:29 +0800

    fix: request

--- src/pages/practice/request.js
+++ src/pages/practice/request.js
@@ -34,30 +34,15 @@ export function getKLine (params) {
   })
 }
 
-export function getFeishuTenatToken () {
+
+export function postFeishuTableData (records) {
   return axios({
     method: 'post',
-    url: 'https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal',
+    url: 'https://post-feu-record-feishu-open-api-pafypimqkx.cn-hangzhou.fcapp.run',
     data: {
-      app_id: urlParams.get('feishu_app_id'),
-      app_secret: urlParams.get('feishu_app_secret')
+      feishu_app_id: urlParams.get('feishu_app_id'),
+      feishu_app_secret: urlParams.get('feishu_app_secret'),
+      records
     }
-  }).then((res) => {
-    return res?.data?.tenant_access_token
-  });
-}
-
-export function postFeishuTableData (data) {
-  return getFeishuTenatToken().then((token) => {
-    axios({
-      method: 'post',
-      url: 'https://open.feishu.cn/open-apis/bitable/v1/apps/bascnA92KPOsGmWsXTpMfRPmAzf/tables/tblnTQeCq0klxdiZ/records/batch_create',
-      headers: {
-        Authorization: `Bearer ${token}`
-      },
-      data: {
-        records: data
-      }
-    })
-  }) 
+  })
 }
